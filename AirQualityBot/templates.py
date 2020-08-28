37281aa271f356c6be536ac3db86670d7847d4a7
yrafalin <31785347+yrafalin@users.noreply.github.com>
2020-08-27 17:17:58 -0700

    Air quality bot commands

---
+++ AirQualityBot/templates.py
@@ -0,0 +1,15 @@
+messages = {
+    'search_failed':,
+    'search_missing':,
+    'home_request_msg':,
+    'here_request_msg'
+}
+
+warnings = {
+    'g': '',
+    'm': ' PM2.5 Warning: People with respiratory disease, such as asthma, should limit prolonged outdoor exertion.',
+    'usg': ' PM2.5 Warning: People with respiratory disease, such as asthma, should limit prolonged outdoor exertion.',
+    'u':,
+    'vu':,
+    'h'
+}
